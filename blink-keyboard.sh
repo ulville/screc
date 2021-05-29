72e4b89a524e4bb192be50bab8dbeed1efa802ce
ulville <50755946+ulville@users.noreply.github.com>
2021-05-29 23:12:27 +0300

    If mouse is not detected blink keyboard backlight

---
+++ blink-keyboard.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+function onExit()
+{
+	gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Keyboard.StepUp
+	echo
+	echo "Okkey Byyyy"
+}
+
+trap onExit EXIT 
+
+while :
+do
+	gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Keyboard.StepDown
+	sleep 0.5
+	gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Keyboard.StepUp
+	sleep 1.5
+done
